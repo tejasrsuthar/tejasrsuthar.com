d91afe4d16de82289ec20b749002c9921a9e6b9d
Tejas Suthar <tejact2007@gmail.com>
2019-09-09 12:07:45 +0530

    :tada: blog update

--- update_website.sh
+++ update_website.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 OUTDIR=docs
 LOCAL_GHOST="http://localhost:2368/"
-wget -r -nH -P $OUTDIR -E -T 2 -np -S --header="accept-encoding: gzip" -k $LOCAL_GHOST && \
+wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCAL_GHOST && \
 git add . && \
 git commit -m ':tada: blog update' &&  \
 git push origin master
