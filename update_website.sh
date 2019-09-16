8f64e67878d391be05926fe17d6f4fb147d0975e
Tejas Suthar <tejact2007@gmail.com>
2019-09-16 19:54:16 +0530

    :tada: blog update

--- update_website.sh
+++ update_website.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 OUTDIR=docs
 LOCAL_GHOST="http://localhost:2368/"
+SITE_PATH="https://tejasrsuthar.com"
 wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCAL_GHOST && \
 git add . && \
 git commit -m ':tada: blog update' &&  \
