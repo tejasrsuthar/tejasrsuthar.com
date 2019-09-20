9627246f5ac99b1eaa6607ff4794f363a0ef10f1
Tejas Suthar <tejact2007@gmail.com>
2019-09-20 11:44:40 +0530

    :tada: update blog

--- update_website.sh
+++ update_website.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 OUTDIR=docs
-rm -rf docs/
 LOCAL_GHOST="http://localhost:2368/"
 SITE_PATH="https://tejasrsuthar.com"
 # wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCAL_GHOST && \
-wget -P $OUTDIR -mpck --user-agent="" -e robots=off -E $LOCAL_GHOST && \
+# wget -P $OUTDIR -mpck --user-agent="" -e robots=off -E $LOCAL_GHOST && \
+rm -rf $OUTDIR && gssg --url $SITE_PATH --dest $OUTDIR
 git add . && \
-git commit -m ':tada: blog update' &&  \
+git commit -m ':tada: update blog' &&  \
 git push origin master
