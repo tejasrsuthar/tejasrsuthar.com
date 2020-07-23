07dabf12d3317af4c40f7269c8fff60159278e21
Tejas Suthar <tejact2007@gmail.com>
2020-07-23 10:08:00 +0530

    Feat: update blog :tada:

--- update-website.sh
+++ update-website.sh
@@ -5,14 +5,8 @@ SITE_PATH="https://tejasrsuthar.com"
 
 # remove docs first and add generate the links
 rm -rf $OUTDIR && gssg --url $SITE_PATH --dest $OUTDIR && \
-
-# required for gitHub Pages
 echo "tejasrsuthar.com" > "$OUTDIR/CNAME" && \
 git add . && \
 git commit -m 'Feat: update blog :tada:' &&  \
-
-# tag the release
 npm run release && \
-
-# push the code to repo
 git push origin master
