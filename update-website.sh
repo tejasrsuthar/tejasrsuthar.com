83989cddd0a59e19595b9e5e58a78e8f11a7472d
Tejas Suthar <tejact2007@gmail.com>
2020-07-23 10:05:52 +0530

    Feat: update blog :tada:

--- update-website.sh
+++ update-website.sh
@@ -2,11 +2,17 @@
 OUTDIR=docs
 LOCAL_GHOST="http://localhost:2368/"
 SITE_PATH="https://tejasrsuthar.com"
-# wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCAL_GHOST && \
-# wget -P $OUTDIR -mpck --user-agent="" -e robots=off -E $LOCAL_GHOST && \
+
+# remove docs first and add generate the links
 rm -rf $OUTDIR && gssg --url $SITE_PATH --dest $OUTDIR && \
+
+# required for gitHub Pages
 echo "tejasrsuthar.com" > "$OUTDIR/CNAME" && \
 git add . && \
 git commit -m 'Feat: update blog :tada:' &&  \
-"release": "git add . && standard-version -a"
+
+# tag the release
+npm run release && \
+
+# push the code to repo
 git push origin master
